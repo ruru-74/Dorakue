0836d98f0e000fe8e0324c7abedd680518668689
__Ruru__74 <rokabayashi2004@gmail.com>
2021-10-19 16:37:50 +0900

    first

---
+++ Assets/Scripts/PlayFab/MainPlayFab.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.Json;
+using PlayFab.ClientModels;
+public class MainPlayFab : MonoBehaviour
+{
+    public static List<Level_NormalJob> Level_NormalJob { get; set; }
+    public static List<CatalogItem> Item_Catalog { get; set; }
+    private GetPlayerCombinedInfoRequestParams InfoRequestParams;
+    private void Start()
+    {
+        LoginPlayFab();//ログイン処理
+    }
+    private void LoginPlayFab()
+    {
+        PlayFabClientAPI.LoginWithCustomID(
+            new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true }
+        , result => OnLogin()
+        , error => Debug.Log("ログイン失敗"));
+    }
+    private void OnLogin()
+    {
+        GetItemData();
+    }
+    private void GetItemData()
+    {
+        PlayFabClientAPI.GetCatalogItems(
+            new GetCatalogItemsRequest()
+            , result =>
+            {
+                Item_Catalog = result.Catalog;
+            }
+            , error => Debug.Log(error.GenerateErrorReport()));
+    }
+    private void GetLevelData()
+    {
+        PlayFabClientAPI.GetTitleData(
+            new GetTitleDataRequest()
+            , result => 
+            { 
+                Level_NormalJob = PlayFabSimpleJson.DeserializeObject<List<Level_NormalJob>>(result.Data["Level_NormalJob"]);             
+            }
+            , error => Debug.Log(error.GenerateErrorReport()));
+    }
+}
+[System.Serializable]
+public class Level_NormalJob
+{
+    public int Level;
+    public int Exp_Difference;
+    public int Exp_Cumulative;
+    public int Sp_Difference;
+    public int Sp_Cumulative;
+}
---
+++ Assets/UI/Scripts/ScrollViewController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewController : MonoBehaviour
+{
+    public GameObject scrollView;
+    public GameObject scrollContentPrefab;
+    private void Start()
+    {
+        var viewPort = scrollView.transform.Find("Viewport");
+        Transform content = viewPort.transform.Find("Content");
+
+        // テキストを表示する
+        for (int i = 0; i < MainPlayFab.Item_Catalog.Count; i++)
+        {
+            GameObject contents = Instantiate(scrollContentPrefab,content);
+            contents.name = "Content" + (i+1);
+            var butttonState = contents.GetComponent<Button>();
+            // 背景の色を変えて見やすくする
+            var colors = butttonState.colors;
+            if (i % 2 == 0)
+            {
+                colors.normalColor = new Color(0F / 255F, 0F / 255F, 0F / 255F, 128F / 255F);
+                colors.highlightedColor = new Color(0F / 255F, 0F / 255F, 0F / 255F, 128F / 255F);
+                colors.pressedColor = new Color(0F / 255F, 0F / 255F, 0F / 255F, 128F / 255F);
+                colors.disabledColor = new Color(0F / 255F, 0F / 255F, 0F / 255F, 128F / 255F);
+            }
+            else
+            {
+                colors.normalColor = new Color(0 / 255F, 0 / 255F, 0 / 255F, 50 / 255F);
+                colors.highlightedColor = new Color(0 / 255F, 0 / 255F, 0 / 255F, 50 / 255F);
+                colors.pressedColor = new Color(0 / 255F, 0 / 255F, 0 / 255F, 50 / 255F);
+                colors.disabledColor = new Color(0 / 255F, 0 / 255F, 0 / 255F, 50 / 255F);
+            }
+            butttonState.colors = colors;
+
+
+            var textChild = contents.GetComponentInChildren<Text>();
+            textChild.text = MainPlayFab.Item_Catalog[i].DisplayName;
+        }
+    }
+}
